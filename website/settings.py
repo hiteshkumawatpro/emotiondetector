e9f9efbb8b0a7ea9aa70e926e527d90d61c7089f
Hitesh-Kumawat <77722116+hiteshkumawatpro@users.noreply.github.com>
2022-05-27 01:21:52 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--^yy_f-98bcf!6j473ey$#3=!v3e0n%ytf-nmug&1gxsh7z0$r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','emotionreader.herokuapp.com']
 
 
 # Application definition
